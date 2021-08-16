2192fdca39152d86f0d710dbce2257d7c1deebb4
jasontcrabtree <jasontcrabtree@gmail.com>
2021-08-16 19:59:56 +1200

    setup new project clibboard with deps

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,16 +4,16 @@ import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <div>
       <Head>
-        <title>NextJS Base | Lodestone Studio</title>
+        <title>Clipboard</title>
         <link rel="shortcut icon" href="/Favicon.svg" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <div>
+      <div className="bg-yellow-200 h-screen p-4 max-w-4xl">
         <Component {...pageProps} />
       </div>
-    </>
+    </div>
   );
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,12 @@
 export default function Home() {
   return (
     <main>
-      <h1>Tiny JS Starter</h1>
-      <p>This is a Next JS template setup with files how I prefer</p>
+      <h1 className="font-bold text-4xl text-center pt-24 pb-2">
+        Tiny JS Starter
+      </h1>
+      <p className="">
+        This is a Next JS template setup with files how I prefer
+      </p>
     </main>
   );
 }
