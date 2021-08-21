aea32824f8ec447d2f3ce95a2ce7c0b27df56e0d
jasontcrabtree <jasontcrabtree@gmail.com>
2021-08-21 23:23:03 +1200

    cleanup

--- src/components/AllDailyEntries.js
+++ src/components/AllDailyEntries.js
@@ -7,87 +7,24 @@ import { fetcher } from '../lib/utils';
  * @returns {function} JSX Component
  */
 function AllDailyEntries({ limit = 1 }) {
-  const ALL_ARTICLES_PAGINATED = `query ALL_ARTICLES_PAGINATED($limit: Int = 4) {
-    articles(limit: $limit, order_by: {created_at: desc}) {
-      id
-      title
-      content
-    }
-  }`;
-
-  // const { data, error } = useSWR(
-  //   `query ALL_ARTICLES_PAGINATED {
-  //       articles {
-  //         id
-  //         title
-  //         content
-  //       }
-  //     }`,
-  //   fetcher
-  // );
-
-  // Working stats
-  // const { data, error } = useSWR(
-  //   `query getAll {
-  //     articles {
-  //       id
-  //       title
-  //       content
-  //     }
-  //   }`,
-  //   fetcher
-  // );
-
-  // const variables = { limit: 10 };
-  // const paginateFetcher = (query) => request('/api/paginate', query, variables);
-  const paginateFetcher = (query) => request('/api/paginate', query);
-
-  // const { data, error } = useSWR(
-  //   [
-  //     `query getAll($limit: Int = 4) {
-  //     articles(limit: $limit}) {
-  //       id
-  //       title
-  //       content
-  //     }
-  //   }`,
-  //     { limit: 10 },
-  //   ],
-  //   paginateFetcher
-  // );
-
   // Don’t do this! Deps will be changed on every render.
   // useSWR(['/api/user', { id }], query);
-
   // Instead, you should only pass “stable” values.
   // useSWR(['/api/user', id], (url, id) => query(url, { id }));
-
-  /*  const {
-    data,
-    error,
-  } = useSWR( // useSWR([`query getAll($limit: Int = 4) {
-       articles(limit: $limit}) {
-         id
-         title
-         content}
-        }`,
-      { limit: 10 },
-  ], (url, id) => query(url, { id })); */
-
+  // const variables = { limit: 2 };
+  // const { data, error } = useSWR(
   //   [
-  //     `query getAll($limit: Int = 4) {
-  //     articles(limit: $limit}) {
+  //     `query getAll {
+  //     articles(limit: ${limit}, order_by: {created_at: desc}) {
   //       id
   //       title
   //       content
   //     }
   //   }`,
-  //     { limit: 10 },
+  //     variables,
   //   ],
-  //   paginateFetcher
+  //   () => fetcher({ variables })
   // );
-
-  // const { data, error } = useSWR([ALL_ARTICLES_PAGINATED], paginateFetcher);
 
   // This is probably incorrect. Because we are using API routes to securely access data (via the fetcher function), and I don't want to change the graphql endpoint for each function, we need to change the query client side, not pass in variables, for SWR, I guess? This is the only way I can get it to work for SWR at least.
   const { data, error } = useSWR(
