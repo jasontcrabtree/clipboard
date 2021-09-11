36bbc89e6f102154a5122a4e0a4444458847cf97
jasontcrabtree <jasontcrabtree@gmail.com>
2021-09-11 12:16:25 +1200

    save point

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,10 @@
 import Head from 'next/head';
 import { AppProps } from 'next/app';
-import { Provider } from 'next-auth/client';
+import { Provider, signIn } from 'next-auth/client';
 import '../styles/reset.css';
 import '../styles/globals.css';
 import PrimaryNav from '../components/PrimaryNav';
+import { HiUser } from 'react-icons/hi';
 
 /**
  * Global wrapper component that holds meta value, shared layout styles, shared components (e.g. primary nav). Each page component is wrapped with this component, via the Component + pageProps NextJS functionality.
@@ -22,8 +23,24 @@ function MyApp({
         <link rel="shortcut icon" href="/Favicon.svg" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <PrimaryNav />
-      <main className="bg-gray-900 min-h-screen h-auto w-screen/10 p-4 pb-48 flex justify-center leading-loose text-gray-500">
+      <main className="bg-gray-900 min-h-screen h-auto w-screen/10 p-4 pb-48 flex justify-center flex-col leading-loose text-gray-500">
+        {session ? (
+          <PrimaryNav />
+        ) : (
+          <div className="bg-gray-900 text-gray-200 place-content-center">
+            <a
+              className="max-w-3xl w-96 mx-auto flex content-center justify-center"
+              href="/api/auth/signin"
+              onClick={(e) => {
+                e.preventDefault();
+                signIn();
+              }}
+            >
+              <HiUser size="24" />
+              Log in
+            </a>
+          </div>
+        )}
         <div className="max-w-3xl w-96 mx-auto">
           <Component {...pageProps} />
         </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,5 @@
-// import AllEntries from '../components/AllEntries';
 import { useSession } from 'next-auth/client';
-import { string } from 'prop-types';
-import KBShortcutsTooltip from '../components/KBShortcutsTooltip';
 import UserDailyEntries from '../components/UserDailyEntries';
-
-interface NewType {
-  props: {
-    data: object;
-  };
-  user: string;
-}
-
-// eslint-disable-next-line require-jsdoc
-// export async function getServerSideProps(): Promise<NewType> {
-//   const userQuery = `query findUser { users(where: {id: {_eq: "26457118"}})
-//         { id, user_id }}`;
-
-//   const res = await fetch(process.env.GRAPHQL_ENDPOINT, {
-//     method: 'POST',
-//     headers: {
-//       'x-hasura-admin-secret': process.env.HASURA_ADMIN_SECRET,
-//     },
-//     body: JSON.stringify({ query: userQuery }),
-//   });
-
-//   const data = await res.json().catch((error) => console.log(error));
-
-//   return { props: { data } };
-// }
 
 /**
  * Homepage component, shows HTML on the index page. Each component has it's own self contained logic.
@@ -35,37 +7,21 @@ interface NewType {
  * @returns {function} JSX Stateless Component
  */
 export default function Home(): JSX.Element {
-  // const user = data?.data?.data?.users[0].user_id;
-
   const [session, loading] = useSession();
 
-  // session
-  //   ? console.log(session.user_id?.data?.users[0].user_id)
-  //   : console.log(loading);
+  if (!session) return null;
 
-  if (!session) return <div>Loading</div>;
-
-  console.log(`Session`, session);
+  if (loading) return <div>Loading</div>;
 
   // @ts-ignore
   const user = session.user_id.data.users[0].user_id;
-  console.log(user);
 
   return (
     <section className="flex flex-col w-full gap-8">
       <h1 className="font-bold text-4xl text-center pt-8 pb-2 mb-8 text-blue-400">
         Clipboard
       </h1>
-      {/* <AllDailyEntries limit={3} /> */}
-      {/* <UserDailyEntries userId={`123`} /> */}
-      {/* {session.token ? (
-        <UserDailyEntries userId={user} />
-      ) : (
-        <div>Log in to view data</div>
-      )} */}
       <UserDailyEntries userId={user} />
-      {/* <AllEntries /> */}
-      {/* <KBShortcutsTooltip /> */}
     </section>
   );
 }
